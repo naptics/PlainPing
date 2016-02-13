c2bfd7adec807a0e6b242cc5f6ace4642f015108
Jonas Schoch <jonas.schoch@naptics.ch>
2016-02-13 15:00:44 +0100

    add tests

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -14,16 +14,43 @@ class Tests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+    func testPingValidUrl() {
+        let url = "www.google.com"
+        let timeout = 3.0
+        
+        let readyExpectation = expectationWithDescription("ready")
+        
+        PlainPing.ping(url, withTimeout: timeout, completionBlock: { (timeElapsed:Double?, error:NSError?) in
+            XCTAssertNotNil(timeElapsed)
+            XCTAssertLessThanOrEqual(timeElapsed!/1000, timeout)
+            XCTAssertNil(error)
+            
+            readyExpectation.fulfill()
+        })
+        
+        // Loop until the expectation is fulfilled
+        waitForExpectationsWithTimeout(5, handler: { error in
+            XCTAssertNil(error, "Error")
+        })
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
+    func testPingInvalidUrl() {
+        let url = "192.168.1.432"
+        let timeout = 3.0
+        
+        let readyExpectation = expectationWithDescription("ready")
+        
+        PlainPing.ping(url, withTimeout: timeout, completionBlock: { (timeElapsed:Double?, error:NSError?) in
+            XCTAssertNil(timeElapsed)
+            XCTAssertNotNil(error)
+            
+            readyExpectation.fulfill()
+        })
+        
+        // Loop until the expectation is fulfilled
+        waitForExpectationsWithTimeout(5, handler: { error in
+            XCTAssertNil(error, "Error")
+        })
     }
     
 }
