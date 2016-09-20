b9c1a7a5cd3711eb1a194698d28f8777b0aade70
Jonas Schoch <jonas.schoch@naptics.ch>
2016-09-20 10:44:41 +0200

    gnore pods folder

--- Example/Pods/Target Support Files/PlainPing/PlainPing-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "SimplePing.h"
-
-FOUNDATION_EXPORT double PlainPingVersionNumber;
-FOUNDATION_EXPORT const unsigned char PlainPingVersionString[];
-
--- Example/Pods/Target Support Files/Pods-PlainPing_Tests/Pods-PlainPing_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PlainPing_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_PlainPing_Tests
-@end
